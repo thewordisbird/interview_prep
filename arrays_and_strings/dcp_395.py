b9854bceff36daaf0be99c4d276c0abac591eb71
Justin Bird <justin@thewordisbird.dev>
2019-12-24 09:28:01 -0800

    dcp 395. added notes and more tests

--- arrays_and_strings/dcp_395.py
+++ arrays_and_strings/dcp_395.py
@@ -12,8 +12,19 @@
 # and M is the number of letters in each word.
 # The space complexity is O(N) as a hash table is required which will hold all the words
 
+# This algorithm assumes words will be all alpha and doesn't have any checks otherwise. 
+#   though, as it is simply using ascii values, any value in the ascii table would work
+#   with a modification to the encode_word function to check if it's alphabetic before
+#   running the lower() method on it. 
+
 
 def group_anagrams(words):
+    '''Returns anagrams grouped together'''
+    # Words are encoded to have a key unique to an anagram.
+    # This key is stored in the anagram_hash and words with 
+    #   the same anagram_key are added to the value list as they
+    #   are seen. 
+    # The values in the anagram_hash are returned as the result
     anagram_hash = {}
     for word in words:
         key = encode_word(word)
@@ -25,6 +36,8 @@ def group_anagrams(words):
     return [v for _,v in anagram_hash.items()]
 
 def encode_word(word):
+    '''Sums ascii codes of each letter and appends the word
+        length to form a key unique to an anagram'''
     ascii_sum = 0
     for letter in word:
         ascii_sum += ord(letter.lower())
@@ -34,4 +47,12 @@ def encode_word(word):
 if __name__ == "__main__":
     words = ['eat', 'ate', 'apt', 'pat', 'tea', 'now']
     result = [['eat', 'ate', 'tea'], ['apt', 'pat'], ['now']]
+    print(group_anagrams(words) == result)
+
+    words = ['eAt', 'ate', 'apt', 'Pat', 'tea', 'now']
+    result = [['eAt', 'ate', 'tea'], ['apt', 'Pat'], ['now']]
+    print(group_anagrams(words) == result)
+
+    words = ['eAts', 'ate', 'apt', 'Pat', 'tea', 'now', 'SEAT']
+    result = [['eAts', 'SEAT'], ['ate', 'tea'], ['apt', 'Pat'], ['now']]
     print(group_anagrams(words) == result)
