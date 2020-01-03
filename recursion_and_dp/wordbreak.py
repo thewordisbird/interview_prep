b6354a26627404599aa24af4441866ca2264ac3a
Justin Bird <justin@thewordisbird.dev>
2020-01-03 14:25:19 -0800

    fixed recursive solution. DP not working

--- recursion_and_dp/wordbreak.py
+++ recursion_and_dp/wordbreak.py
@@ -1,16 +1,41 @@
 def wb(str, dict):
+    # Goal: If all sequences have been found in the dict return true
     if len(str) == 0:
         return True
     
-    for i in range(1, len(str) + 1):
+    # Choices: look at all substrings in string
+    for i in range(len(str)+1):
+        # Constraints: is the substring in the dictionary?
         print(f'checking {str[:i]} in {dict} -> {str[:i] in dict}')
-        if str[:i] in dict:
-            return wb(str[i:], dict)
+        if str[:i] in dict and wb(str[i:], dict):
+            return True
 
+    # No solution
     return False
+
+def wb_dp(str, dict, memo=set()):
+    if len(str) == 0:
+        return True
+
+    if str in memo:
+        return True
+
+    for i in range(len(str)+1):
+        # Constraints: is the substring in the dictionary?
+        print(f'checking {str[:i]} in {dict} -> {str[:i] in dict}')
+        if str[:i] in dict and wb_dp(str[i:], dict, memo):
+            memo.add(str[:i])
+            return True
+
+    # No solution
+    return False
+    
+
+
 
 if __name__ == "__main__":
     d = {"i", "like", "sam", "sung", "samsung", "mobile", "ice", "cream", "icecream", "man", "go", "mango"}
-    s = 'iceream'
+    s = 'samsam'
     print('running')
     print(wb(s, d))
+    print(wb_dp(s, d))
