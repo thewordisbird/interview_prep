d7e2d6aa00f2546c31016fcafdfb918faba30c8a
Justin Bird <justin@thewordisbird.dev>
2020-01-10 14:47:47 -0800

    permutations of a list

--- recursion_and_dp/permutations.py
+++ recursion_and_dp/permutations.py
@@ -1,15 +1,24 @@
 
 
-def perm(nums):
-    result = []
-    if len(nums) == 1:
-        result.append(nums[0])
-    else:
-        for i,n in enumerate(nums):
-            result = [n] + perm(nums[:i] + nums[i+1:])
+def permutations(nums, perm=[], perms=[]):
+    # Base case: An empty list is passed in. Add the perm list
+    # to the perms list. 
+    if len(nums) == 0:
+        # In order to avoid appending the list by refrence, we need to append list[:].
+        if perm:
+            perms.append(perm[:])
     
-    return result
+    for i,n in enumerate(nums):
+        perm.append(n)
+        permutations(nums[:i] + nums[i+1:], perm, perms)
+        # Backtrack by removing the last item added to perm
+        perm.pop()
+    
+    return perms
 
 if __name__ == "__main__":
     nums = [1, 2, 3]
-    print(perm(nums))
+    #print(permutations(nums))
+
+    nums = [1,2]
+    print(permutations(nums))
