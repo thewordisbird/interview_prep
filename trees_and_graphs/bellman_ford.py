ed2d3bd2c637e85e6f0252c23161b8274765a829
Justin Bird <justin@thewordisbird.dev>
2020-03-09 13:12:39 -0700

    bellman-ford implementation

--- trees_and_graphs/bellman_ford.py
+++ trees_and_graphs/bellman_ford.py
@@ -3,12 +3,37 @@
 def bellman_ford(g, s):
     # Initialize datastructures
     d = {k: float('inf') for k in g}
+    p = {k: None for k in g}
     d[s] = 0
     # Iterate thrugh graph maximum of v-1 times
     for i in range(len(g) - 1):
-        for k,v in d.items():
-            if v != float('inf'):
+        for v, wv in d.items():
+            if wv != float('inf'):
                 # Relax edges from v
-                
+                for n,wn in g[v].items():
+                    if (d[v] + wn) < d[n]:
+                        d[n] = d[v] + wn
+                        p[n] = v
+                    
+    #return d, p 
+    
     # Attemp to relax edges one more time. if any can be reduced, there is a negative cycle
+    for v, wv in d.items():
+        if wv != float('inf'):
+            # Relax edges from v
+            for n,wn in g[v].items():
+                if (d[v] + wn) < d[n]:
+                    return False, None, None
 
+    return True, d, p
+
+if __name__ == "__main__":
+    g = {
+            'A': {'B': -1, 'C': 4},
+            'B': {'C': 3, 'D': -2, 'E': 2},
+            'C': {},
+            'D': {'B': 1, 'C':5 },
+            'E': {'D': -3} 
+        }
+
+    print(bellman_ford(g, 'A'))
